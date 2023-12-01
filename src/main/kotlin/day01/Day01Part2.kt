3e05e32c3a53b5c26316553b39dd1a1d5e30f8d2
Malox <Maxi.Junges@gmx.de>
2023-12-01 17:55:35 +0100

    add second solution to Day1Part2

--- src/main/kotlin/day01/Day01Part2.kt
+++ src/main/kotlin/day01/Day01Part2.kt
@@ -4,30 +4,40 @@ import readResourceLines
 
 fun main() {
     val input = readResourceLines("Day01.txt")
-    val solution = solvePart2(input)
+    val solution = solvePart2Version2(input)
     println(solution)
 }
 
-fun solvePart2(input: List<String>) = input
-    .map { line ->
-        line.mapIndexedNotNull { index, _ ->
-            numberMap.mapNotNull { (key, value) ->
-                line[index].digitToIntOrNull()?.let { return@mapIndexedNotNull listOf(it) }
-                val substring = (index + key.length).let { upperbound -> if(upperbound <= line.length) line.substring(index until index + key.length) else return@mapNotNull null  }
-                if(substring.contains(key)) value.toInt() else null
-            }
-        }.flatten().let { (it.first().toString() + it.last()).toInt() }
-    }.sum()
+//fun solvePart2(input: List<String>) = input
+//    .map { line ->
+//        line.mapIndexedNotNull { index, _ ->
+//            numberMap.mapNotNull { (key, value) ->
+//                line[index].digitToIntOrNull()?.let { return@mapIndexedNotNull listOf(it) }
+//                val substring = (index + key.length).let { upperbound -> if(upperbound <= line.length) line.substring(index until index + key.length) else return@mapNotNull null  }
+//                if(substring.contains(key)) value else null
+//            }
+//        }.flatten().let { (it.first().toString() + it.last()).toInt() }
+//    }.sum()
 
+fun solvePart2Version2(input: List<String>): Int {
+    val numbersAndDigits = numberMap + (1..9).toList().associateBy { it.toString() }
+    return input.map { line ->
+        val (_, firstValue)= numbersAndDigits.map { (key, value) -> line.indexOf(key) to value }.filter { it.first != -1 }
+            .minByOrNull { (index, _) -> index }!!
+        val (_, lastValue) = numbersAndDigits.map { (key, value) -> line.lastIndexOf(key) to value }.filter { it.first != -1 }
+            .maxByOrNull { (index, _) -> index }!!
+        firstValue * 10 + lastValue
+    }.sum()
+}
 
 val numberMap = mapOf(
-    "one" to "1",
-    "two" to "2",
-    "three" to "3",
-    "four" to "4",
-    "five" to "5",
-    "six" to "6",
-    "seven" to "7",
-    "eight" to "8",
-    "nine" to "9",
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
 )
